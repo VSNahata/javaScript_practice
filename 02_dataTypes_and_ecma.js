8794fdc5f0e86cc3a23c18030e3a9cb62612e3ff
Varun <varunmj99@gmail.com>
2023-08-30 10:42:55 +0530

    more on dataTypes

---
+++ 02_dataTypes_and_ecma.js
@@ -0,0 +1,17 @@
+"use newer version" // all the code written under the double quotes are 
+// regarded as the newer version of js code
+
+// alert(3+3) -> it will not give result as it is node not browser.
+
+//data types
+// number => 2 to the power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => matlab abhi value assign nahi hui hai
+// symbol => unique
+
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
