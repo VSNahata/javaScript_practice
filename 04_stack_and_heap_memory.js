217d6e46f059a28f3406be7528ab323a0f42d2bd
Varun <varunmj99@gmail.com>
2023-08-30 11:16:33 +0530

    stack and heap memory

---
+++ 04_stack_and_heap_memory.js
@@ -0,0 +1,7 @@
+/*
+stack memory => primitive data types use karte waqt
+heap mimory => non primitive data types use karte waqt
+
+inshort samajhne wise kuch alag nahi hai you can rely on your previous knowlede
+bss upar wala utna sa dhyaan rakhna
+*/
